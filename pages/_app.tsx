cce46eed848a75468aac967af8717ac8989319f2
Abhishek Purty <abhishekpurty1999@gmail.com>
2023-04-26 18:29:22 +0530

    first upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Layout from '@/components/Layout'
+// import Modal from '@/components/Modal' 
+import '@/styles/globals.css'
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    {/* <Modal actionLabel ="Submit" title="New"/> */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
+ 
